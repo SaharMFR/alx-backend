0f5b90626f8be87849bac6d2fce69b62740ab9df
SaharMFR <sweetysahora@gmail.com>
2024-06-26 11:10:51 +0000

    Task-0 0x01

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Defines `BasicCache` class """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
