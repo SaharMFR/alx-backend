f6a8df9b686017a568403da4cc041264d6173ae9
SaharMFR <sweetysahora@gmail.com>
2024-06-27 00:05:31 +0000

    Update in task-5 0x01

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Defines `LFUCache` class """
-from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -9,56 +8,26 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize """
         super().__init__()
-        self.cache_data = OrderedDict()
-        self.frequencies = []
-
-    def reorder_elements(self, mru_key):
-        """Reorders the items in this cache based on the most
-        recently used item.
-        """
-        max_positions = []
-        mru_freq = 0
-        mru_pos = 0
-        ins_pos = 0
-        for i, key_freq in enumerate(self.frequencies):
-            if key_freq[0] == mru_key:
-                mru_freq = key_freq[1] + 1
-                mru_pos = i
-                break
-            elif len(max_positions) == 0:
-                max_positions.append(i)
-            elif key_freq[1] < self.frequencies[max_positions[-1]][1]:
-                max_positions.append(i)
-        max_positions.reverse()
-        for pos in max_positions:
-            if self.frequencies[pos][1] > mru_freq:
-                break
-            ins_pos = pos
-        self.frequencies.pop(mru_pos)
-        self.frequencies.insert(ins_pos, [mru_key, mru_freq])
+        self.frequencies = {}
 
     def put(self, key, item):
         """ Put an item in the cache """
-        if key is not None or item is not None:
-            if key not in self.cache_data:
-                if self.MAX_ITEMS < len(self.cache_data) + 1:
-                    lfu_key, _ = self.frequencies[-1]
-                    self.cache_data.pop(lfu_key)
-                    self.frequencies.pop()
-                    print("DISCARD:", lfu_key)
-                self.cache_data[key] = item
-                ins_index = len(self.frequencies)
-                for i, key_freq in enumerate(self.frequencies):
-                    if key_freq[1] == 0:
-                        ins_index = i
-                        break
-                self.frequencies.insert(ins_index, [key, 0])
-            else:
-                self.cache_data[key] = item
-                self.reorder_elements(key)
+        if key is None or item is None:
+            return
+        if (len(self.cache_data.keys()) == self.MAX_ITEMS
+           and key not in self.cache_data):
+            discarded = min(self.frequencies, key=self.frequencies.get)
+            self.cache_data.pop(discarded)
+            self.frequencies.pop(discarded)
+            print("DISCARD:", discarded)
+        if key in self.cache_data:
+            self.frequencies[key] += 1
+        else:
+            self.frequencies[key] = 1
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
-        if key in self.cache_data:
-            self.reorder_elements(key)
-        return self.cache_data.get(key, None)
+        if key in self.cache_data.keys():
+            self.frequencies[key] += 1
+            return self.cache_data.get(key)
