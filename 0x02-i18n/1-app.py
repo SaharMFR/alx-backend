11b9b15f456d769d90b1af69272cf9d338a63b12
SaharMFR <sweetysahora@gmail.com>
2024-07-02 23:10:38 +0000

    Update in task-1 0x02

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -16,7 +16,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', , methods=['GET'], strict_slashes=False)
+@app.route('/')
 def index() -> str:
     """ Renders `1-index` page """
     return render_template('1-index.html')
