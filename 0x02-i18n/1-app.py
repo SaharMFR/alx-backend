601d97ec9d543b856aaa22e4309bcd30206e8cb5
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2024-07-03 01:42:04 +0300

    Task-1 0x02

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Basic Flask-Babel app """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Configurations for Flask-Babel app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', , methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ Renders `1-index` page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
