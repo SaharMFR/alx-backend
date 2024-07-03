a14f6349734c6ae06f35dade605cf44e25e7cce7
SaharMFR <sweetysahora@gmail.com>
2024-07-03 01:04:52 +0000

    Task-4 0x02

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic Flask-Babel app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Configurations for Flask-Babel app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Forces locale """
+    locale = request.args.get("locale")
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index() -> str:
+    """ Renders `4-index` page """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
